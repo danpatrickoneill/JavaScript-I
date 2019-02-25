d62fa81df5c4ec66e030412d2ecc3ef4c93c851b
danpatrickoneill <dan47573@gmail.com>
2019-02-25 16:58:16 -0500

    Finished exercises

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,20 +5,36 @@
 // };
 // myFunction();
 
+const myFunction = () => console.log("Function was invoked!");
+
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+const anotherFunction = (x) => x;
+
+console.log(anotherFunction(123));
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+const add = (num1, num2) => num1 + num2;
+
+console.log(add(23, 53));
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
+
+const subtract = (num1, num2) => num1 - num2;
+
+console.log(subtract(1000, 7));
 
 
 // Stretch
@@ -28,3 +44,7 @@
 //   return num * 3;
 // });
 // console.log(triple);
+
+const triple = (arr) => arr.map( (num) => num * 3 );
+
+console.log(triple([1,2,3,4]))
